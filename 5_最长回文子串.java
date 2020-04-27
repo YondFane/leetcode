84c9d0f2c95ba8e51648da493a2dd0a613c29a2b
first <1055172199@qq.com>
2020-04-28 00:23:56 +0800

    5、最长回文子串:sparkles:

---
+++ 5_最长回文子串.java
@@ -0,0 +1,59 @@
+package 最长回文子串_5;
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * <p>
+ * 示例 1：
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * <p>
+ * 示例 2：
+ * 输入: "cbbd"
+ * 输出: "bb"
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.longestPalindrome("dfgaaasgd"));
+    }
+}
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        char[] str = s.toCharArray();
+        int[] range = new int[2];
+        for (int i = 0; i < len; i++) {
+            i = fun(str, i, range);
+        }
+
+        return s.substring(range[0], range[1]+1);
+    }
+
+    public int fun(char[] str, int i, int[] range) {
+        int j = i;
+        int len = str.length;
+        // 寻找回文串的中心部分
+        while (j < len - 1 && str[j + 1] == str[i]) {
+            j++;
+        }
+        // 中心部分最右边界值
+        int result = j;
+        // 中心部分左右相等的字符
+        while (i > 0 && j < len - 1 && str[i - 1] == str[j + 1]) {
+            i--;
+            j++;
+        }
+        // 记录最大长度
+        if (j - i > range[1] - range[0]) {
+            range[0] = i;
+            range[1] = j;
+        }
+        return result;
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 [3、无重复字符的最长子串](https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/)
 
 [4、寻找两个有序数组的中位数](https://leetcode-cn.com/problems/median-of-two-sorted-arrays/)
+
+[5、最长回文子串](https://leetcode-cn.com/problems/longest-palindromic-substring/submissions/)
