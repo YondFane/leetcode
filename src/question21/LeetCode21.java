663ff49732a4ad7e77daac8152363844202022c5
YFAN <1055172199@qq.com>
2021-05-09 09:27:17 +0800

    21-添加新解题方法:sparkles:

--- src/question21/LeetCode21.java
+++ src/question21/LeetCode21.java
@@ -64,6 +64,27 @@ class Solution2 {
         }
         return root.next;
     }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(-1);
+        ListNode temp = root;
+        while (l1 !=null && l2 !=null) {
+            if (l1.val <= l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if(l1 != null) {
+            temp.next = l1;
+        } else if(l2 != null){
+            temp.next = l2;
+        }
+        return root.next;
+    }
 }
 class ListNode {
     int val;
