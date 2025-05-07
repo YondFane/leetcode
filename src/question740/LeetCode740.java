5170884cf9efda1a2861bb4f4fb174b2fa9e8c44
YondFane <1055172199@qq.com>
2025-05-07 22:17:47 +0800

    740-删除并获得点数 :sparkles: https://leetcode.cn/problems/delete-and-earn

--- src/question740/LeetCode740.java
+++ src/question740/LeetCode740.java
@@ -47,6 +47,12 @@ public class LeetCode740 {
 
 
 class Solution {
+    /**
+     * 类似
+     198. 打家劫舍
+     你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+     */
     public int deleteAndEarn(int[] nums) {
         // 数组最大的值
         int maxVal = 0;
@@ -77,6 +83,11 @@ class Solution {
         int first = nums[0], second = Math.max(nums[0], nums[1]);
         for (int i = 2; i < size; i++) {
             int temp = second;
+            /**
+             * 如果选择x，无法选择 x-1 或 x+1
+             * 每次选择时，判断当前数字 + 前前数字的和大，还是前一个数字大
+             *
+             */
             second = Math.max(first + nums[i], second);
             first = temp;
         }
